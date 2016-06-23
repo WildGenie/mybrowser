f38d18ddbc9443d8a456d5594929978bcb1e6aa9
Cory Craig <cory.craig@schear.net>
2016-06-23 14:38:30 -0400

    Autostart, finishing touches to prepare for Presence status release

--- NCP Browser V2/Program.cs
+++ NCP Browser V2/Program.cs
@@ -1,24 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IWshRuntimeLibrary;
 
 namespace NCP_Browser
 {
     static class Program
     {
+        private static string shortcutFileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Startup), "Start TestApp.lnk");
+        private static string publisherName = Application.CompanyName;
+        private static string productName = "NCP Web";
+        private static string allProgramsPath = Environment.GetFolderPath(Environment.SpecialFolder.Programs);
+        private static string shortcutPath = Path.Combine(allProgramsPath, publisherName);
+        private static string shortcutFile = Path.Combine(shortcutPath, productName) + ".appref-ms";
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            CefSharp.Cef.EnableHighDPISupport();
-            Application.Run(new Salesforce(true, "https://test.salesforce.com"));
+            Install();
+            if (args.Contains("-L"))
+            {
+                Launch();
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                CefSharp.Cef.EnableHighDPISupport();
+                Application.Run(new Salesforce(true, "https://test.salesforce.com"));
+            }
+            
             //Application.Run(new Jabber.Credentials());
+        }
+
+        public static void Launch()
+        {
+            System.Diagnostics.Process.Start(shortcutFile);
+        }
+
+        public static void Install()
+        {
+            WshShell shell = new WshShell();
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutFileName);
+            shortcut.Description = "New shortcut for a Notepad";
+            shortcut.TargetPath = shortcutFile;
+            shortcut.Save();
+            shortcut.WorkingDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+            shortcut.Arguments = "-l";
+            shortcut.Save();      
         }
     }
 }
